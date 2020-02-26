9dcee77ab56816ac409b174495d6ba5d3b3c2236
Marquis Kurt <software@marquiskurt.net>
2020-02-26 15:02:32 -0500

    [slides] Make page pretty and stuff

--- utils.js
+++ utils.js
@@ -4,6 +4,38 @@ const changeCopyright = () => {
         `&copy; ${new Date().getFullYear()} Marquis Kurt. All rights reserved.`;
 }
 
+function changeSlides() {
+    fetch("current.json")
+        .then(resp => resp.json())
+        .then(data => {
+            let title = document.getElementById("current-title");
+            title.innerHTML = data.current.name
+                ? data.current.name
+                : "No current presentation.";
+
+            let desc = document.getElementById("current-description");
+            desc.innerHTML = data.current.description
+                ? data.current.description
+                : "No description given.";
+
+
+            let button = document.getElementById("current-link");
+            if (data.current.file !== undefined) {
+                button.setAttribute("href", `./presentation/${data.current.file}.html`);
+            }
+            else {
+                button.setAttribute("disabled", true);
+                button.innerHTML = "Cannot play presentation";
+            }
+
+            let all = document.getElementById("all-presentations");
+            if (!data.all.length > 0) {
+                all.innerHTML = "No other presentations are available.";
+            }
+        })
+}
+
 window.onload = () => {
     changeCopyright();
+    changeSlides();
 }
