997258d3e6cafb92bee360993230964e6d264d78
Marquis Kurt <software@marquiskurt.net>
2020-05-12 11:29:41 -0400

    [slides] Add positive anymore

--- utils.js
+++ utils.js
@@ -31,11 +31,43 @@ function changeSlides() {
             let all = document.getElementById("all-presentations");
             if (!data.all.length > 0) {
                 all.innerHTML = "No other presentations are available.";
+            } else {
+                let realInnerHTML = "";
+                for (const {name, description, file} of data.all) {
+                    realInnerHTML += `
+                        <div class="presentation-card">
+                            <div class="presentation-img">
+                                <div class="presentation-icon-bay">
+                                    <i class="presentation-icon" data-feather="monitor"></i>
+                                </div>
+                            </div>
+                            <p class="card-title">${name}</p>
+                            <p class="card-description">${description}</p>
+                            <p>
+                                <a class="btn small" href="./presentation/${file}.html">Watch Now</a>
+                            </p>
+                        </div>
+                    `;
+                }
+                all.innerHTML = "<div class=\"presentation-cards\">" + realInnerHTML + "</div>";
+                if (feather) {
+                    feather.replace({ width: '64px', height: '64px' });
+                 }
             }
         })
+}
+
+function runWebkitModifs() {
+    let header = document.getElementsByClassName("current-container");
+    if (header.length < 1) { return; }
+    header = header[0];
+    if (navigator.userAgent.match(/Safari/)) {
+        header.setAttribute("style", "height:100vh;");
+    }
 }
 
 window.onload = () => {
     changeCopyright();
     changeSlides();
+    runWebkitModifs();
 }
